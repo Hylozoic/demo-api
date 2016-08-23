aa8b313ae919fd514073002b19d5f52916a65892
Declan Hart <dhart@thoughtworks.com>
2016-08-23 11:06:49 +1000

    DH add accept_partial_offer endpoint

--- routes/securities.js
+++ routes/securities.js
@@ -3,6 +3,7 @@ var router = require('koa-router')();
 var config = require('../config');
 var unauthorized = require('../test-data/unauthorized.json');
 var accept = require('../test-data/accept_offer.json');
+var accept_partial = require('../test-data/accept_partial_offer.json');
 
 router.get('/offers/accept/:id', function *(next, req) {
     if(this.headers['authorization'] === config.bearerToken && this.params.id == 1){
@@ -15,4 +16,14 @@ router.get('/offers/accept/:id', function *(next, req) {
     }
 });
 
+router.get('/offers/accept/partial/:offerId/:numShares', function * (next, req) {
+  if(this.headers['authorization'] === config.bearerToken && this.params.offerId == 1) {
+    this.status = config.okResponse;
+    this.body = accept_partial;
+  }
+  else {
+      this.status = config.unauthorized;
+      this.body = unauthorized;
+  }
+});
 module.exports = router;
