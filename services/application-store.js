555ac9b1d19c816e9c740eda11177dd7adf19995
Jessie Wei <jessieweiyi@gmail.com>
2016-09-01 08:50:41 +1000

    modified the application to work on multiple users

--- services/application-store.js
+++ services/application-store.js
@@ -7,13 +7,16 @@ const NAME_OFFERS = 'offers'
 const NAME_LOGS = 'logs'
 const NAME_SYNDICATE = 'syndicate'
 const NAME_WALLET = 'wallet'
+const NAME_WALLET_LOCK = "wallet_lock"
 
-const getRedisClient = function () {
+const getRedisClient = function (isSync) {
   const url = process.env.REDIS_URL || 'redis://localhost:6379'
   const redisInfo = require('parse-redis-url')(redis).parse(url)
   const client = redis.createClient(redisInfo)
-  bluebird.promisifyAll(redis.RedisClient.prototype)
-  bluebird.promisifyAll(redis.Multi.prototype)
+  if(!isSync){
+    bluebird.promisifyAll(redis.RedisClient.prototype)
+    bluebird.promisifyAll(redis.Multi.prototype)
+  }
   return client
 }
 
@@ -30,7 +33,7 @@ module.exports = {
       })
   },
 
-  set: function(redisKey, redisHash, obj){
+  set: function(redisKey, redisHash, obj, callback){
     const client = getRedisClient()
     client.hset(redisKey, redisHash, JSON.stringify(obj))
     client.quit()
@@ -86,5 +89,46 @@ module.exports = {
 
   getWallet: function (userId) {
     return this.get(NAME_WALLET, userId)
+  },
+
+  updateWallet: function(userId, amount, direction, callback){
+    return new Promise((resolve, reject) => {
+      const client = getRedisClient()
+      client.watch(NAME_WALLET)
+      return client.hmgetAsync(NAME_WALLET, userId).then((res) => {
+          var wallet = JSON.parse(res)
+          var etherAmount = amount * 1000000000000000000
+          var balance;
+          if (direction === 'out') {
+            wallet.walletDetails.latest.amount
+            = wallet.walletDetails.latest.amountAvailable
+            = wallet.walletDetails.latest.amountOnHold
+            = wallet.walletDetails.pending.amount
+            = wallet.walletDetails.pending.amountAvailable
+            = wallet.walletDetails.pending.amountOnHold
+            = wallet.walletDetails.latest.amount - etherAmount
+          } else if (direction === 'in') {
+            wallet.walletDetails.latest.amount
+            = wallet.walletDetails.latest.amountAvailable
+            = wallet.walletDetails.latest.amountOnHold
+            = wallet.walletDetails.pending.amount
+            = wallet.walletDetails.pending.amountAvailable
+            = wallet.walletDetails.pending.amountOnHold
+            = wallet.walletDetails.latest.amount + etherAmount
+          }
+
+          return wallet
+      })
+      .then((wallet) => {
+        client.hset(NAME_WALLET, userId, JSON.stringify(wallet), (err, res) => {
+          if(callback){
+            callback
+          }
+          client.unwatch()
+          client.quit()
+          resolve()
+        })
+      })
+    })
   }
 }
--- services/wallet.js
+++ services/wallet.js
@@ -22,7 +22,7 @@ const create =function(walletId, userId, balance){
 module.exports = {
   createDemoWallets : function() {
     create(1, 9, 5000000000000000000000);
-    create(2, 49, 3458967894567890987500);
+    create(2, 49, 3000000000000000000000);
     create(3, 78, 80000000000000000000);
     create(3, 18, 1000000000000000000);
     create(3, 27, 3918000000000000000000);
@@ -37,18 +37,7 @@ module.exports = {
     return ApplicationStore.getWallet(userId);
   },
 
-  updateWallet : function (user_id, amount, direction) {
-    return ApplicationStore.getWallet(user_id).then((wallet) => {
-      var etherAmount = amount * 1000000000000000000
-      var balance;
-      if (direction === 'out') {
-        balance = wallet.walletDetails.latest.amount - etherAmount
-        create(wallet.walletId, user_id, balance)
-      } else if (direction === 'in') {
-        balance = wallet.walletDetails.latest.amount + etherAmount
-        create(wallet.walletId, user_id, balance)
-      }
-      return
-    })
+  updateWallet : function (userId, amount, direction, callback) {
+    return ApplicationStore.updateWallet(userId, amount, direction, callback);
   }
 }
